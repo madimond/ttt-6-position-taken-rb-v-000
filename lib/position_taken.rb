bb951988d7234b6e462f07a2dff6f1450ce38eb1
Margaret Dimond <margaretdimond@gmail.com>
2018-05-17 18:16:59 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+board = Array.new(9, " ")
+def position_taken?(board, index)
+  if board[index] == " " || "" || nil
+    return false
+  elsif board[index] == !(" " || "" || nil)
+    return true
+  else nil
+  end
+end
