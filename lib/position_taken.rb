2828690ffb65e07d5f57541e9879d09c3e7eb3ae
Margaret Dimond <margaretdimond@gmail.com>
2018-05-17 18:41:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 board = Array.new(9, " ")
 def position_taken?(board, index)
   if board[index] != ("X" || "O")
-    return false
-  else board[index] == ("X" || "O")
+  else  board[index] == "X" || board[index] == "O"
   end
 end
